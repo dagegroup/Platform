c560edc73bb3dc38a6317b32d899c3a43c8b09ba
chono <602486761@qq.com>
2018-11-29 15:27:42 +0800

    Initial commit

---
+++ src/main/java/com/dage/controller/Test.java
@@ -0,0 +1,13 @@
+package com.dage.controller;
+
+/**
+ * className:Test
+ * discription:
+ * author:lc
+ * createTime:2018-11-29 15:09
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
