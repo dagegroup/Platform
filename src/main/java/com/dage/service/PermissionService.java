a589eb5c7490d1de53823131cfaafdeeb2973199
Promenkey-k <15660003543@163.com>
2018-12-07 15:31:48 +0800

    权限

---
+++ src/main/java/com/dage/service/PermissionService.java
@@ -0,0 +1,10 @@
+package com.dage.service;
+
+/**
+ * @className:PermissionService
+ * @discription:
+ * @author:ProMonkey-K
+ * @creatTime:2018-12-07 15:29
+ */
+public interface PermissionService {
+}
