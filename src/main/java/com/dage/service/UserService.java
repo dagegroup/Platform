36540a9e70f8f108bf937c59010fcd74211308c7
QYChenMin <2019131420@qq.com>
2018-12-14 11:29:05 +0800

    页面修改

---
+++ src/main/java/com/dage/service/UserService.java
@@ -0,0 +1,14 @@
+package com.dage.service;
+
+import java.util.Map;
+
+/**
+ * className:UserService
+ * discription:
+ * author:ChenMing
+ * creatTime:2018-12-10 16:41
+ */
+public interface UserService {
+
+    Map  getByuserName(String telephone,String password);
+}
