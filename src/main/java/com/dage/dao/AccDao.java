dd4acde168d09e2175876e1c8a721f11494e901c
QYChenMin <2019131420@qq.com>
2018-12-25 11:01:53 +0800

    Merge branch 'master' of https://github.com/dagegroup/Platform
    # Conflicts:
    #	src/main/resources/templates/reception/借款信息填写2.html

--- src/main/java/com/dage/dao/AccDao.java
+++ src/main/java/com/dage/dao/AccDao.java
@@ -12,11 +12,11 @@ import java.util.Map;
  * creatTime:2018-12-22 10:19
  */
 public interface AccDao {
-    @Select("<script>select * from tb_system_account_flow <where>" +
+    @Select("<script>select a.*,b.name from tb_system_account_flow a ,tb_emp b where a.orderid=b.phone  " +
             " <if test=\"USERID!=null and USERID!=''\"> and userid=#{USERID}</if>" +
             " <if test=\"FLOWTYPE!=null and FLOWTYPE!=''\"> and flowtype like '%'||#{FLOWTYPE}||'%'</if>" +
             " <if test=\"FLOWDATE!=null and FLOWDATE!=''\">  and flowdate like '%'||to_date(#{FLOWDATE},'yyyy-mm-dd')||'%'</if>" +
-            " </where></script>")
+            "</script>")
     List<Map> getList(Map map);
     @Select("select flowtype from tb_system_account_flow group by flowtype ")
     List<Map> getFlowtype();
