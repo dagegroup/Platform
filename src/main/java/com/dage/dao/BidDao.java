67c966eb1d8f51564575994fded1383f7fae30bb
chono <602486761@qq.com>
2018-12-27 09:46:49 +0800

    借款进度

--- src/main/java/com/dage/dao/BidDao.java
+++ src/main/java/com/dage/dao/BidDao.java
@@ -22,7 +22,7 @@ public interface BidDao {
      */
     @Select("<script> " +
             "select bidid,userid,auditid,bidproject,bidamount,bidcurrentamount,bidrepaymentmethod,bidrate," +
-            "100*round(bidcurrentamount/bidamount,4)||'%' as bidschedule,biddeadline,bidissuedate,biddeadday,bidapplydate," +
+            "nvl(100*round(bidcurrentamount/bidamount,4),0)||'%' as bidschedule,biddeadline,bidissuedate,biddeadday,bidapplydate," +
             "biddeaddate,biddesc,bidtype,bidstate from bid_info " +
             "where bidstate='待投标' " +
             "<if test=\" bidrate!=null and bidrate!=''\"> and ${bidrate}</if>" +
